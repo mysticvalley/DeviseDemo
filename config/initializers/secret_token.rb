2196b6dfcf05bc373e801df6aa02a7bb18d36664
pratuat <pratuat@pratuat-System-Product-Name.(none)>
2011-12-05 20:21:54 +0545

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tour::Application.config.secret_token = 'c25f696818d38df44d1155c88355b7e6150424260e19c3b4ff0966db58aad151523f144cb52c0befebb886c21f69b28ee449ce505676605251a18f4002a76f3a'
